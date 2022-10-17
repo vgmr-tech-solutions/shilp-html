c8075aae0ac5d12d43066b8b94c429719a7baa40
tanuja-medge-sublimis <tanuja.medge@sublimis.tech>
2022-10-17 10:54:14 +0530

    changes done

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,7 +10,7 @@ $('#menu').click(function () {
 
 // topnav
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 10) {
+    if ($(this).scrollTop() > 148) {
         $('header').addClass('fixed-header');
     } else {
         $('header').removeClass('fixed-header');
